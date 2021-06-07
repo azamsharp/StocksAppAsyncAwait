9263d224e659a8571fb486143f660b9bf5bcfa82
Mohammad Azam <azamsharp@gmail.com>
2021-06-06 22:36:27 -0500

    Initial Commit

---
+++ Learn/LearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnApp.swift
+//  Learn
+//
+//  Created by Mohammad Azam on 6/6/21.
+//
+
+import SwiftUI
+
+@main
+struct LearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
